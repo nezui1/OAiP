9c5873f794d58d5bcd9f20b0b773206adb24c034
nezui1 <lalaevexpress@gmail.com>
2023-11-03 11:17:51 +0400

    ss

---
+++ laba14/WindowsProject1/Images.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <windows.h>
+
+void Image0(HDC hdc, int cx, int cy, COLORREF color);
+void Image1(HDC hdc, int cx, int cy, COLORREF color);
+void Image2(HDC hdc, int cx, int cy, COLORREF color);
+void Image3(HDC hdc, int cx, int cy, COLORREF color);
+void Image4(HDC hdc, int cx, int cy, COLORREF color);
+void Image5(HDC hdc, int cx, int cy, COLORREF color);
+//void Image6(HDC hdc, int cx, int cy, COLORREF color);
+//void Image7(HDC hdc, int cx, int cy, COLORREF color);
+//void Image8(HDC hdc, int cx, int cy, COLORREF color);
+//void Image9(HDC hdc, int cx, int cy, COLORREF color);
